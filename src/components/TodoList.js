16aa69cc1df866a09af7bcb9bedef5ea782017a7
Pawan <pawan@gmail.com>
2025-10-19 11:15:50 +0530

    done

---
+++ src/components/TodoList.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const TodoList = ({ todos, handleComplete }) => {
+  return (
+    <div>
+      <h2>Child Component</h2>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <span
+              style={{
+                textDecoration: todo.completed ? "" : "none",
+              }}
+            >
+              {todo.text}
+            </span>
+            {!todo.completed && (
+              <button onClick={() => handleComplete(todo.id)}>Complete</button>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
