ccecfcad2b27e9bd39a39efa3ba541cbeef460a1
Pawan <pawan@gmail.com>
2025-10-19 11:19:41 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,8 @@ import TodoList from "./TodoList";
 const App = () => {
   const [todos, setTodos] = useState([
     { id: 1, text: "Learn React", completed: false },
-    { id: 2, text: "Build a React App", completed: false },
-    { id: 3, text: "Deploy the React App", completed: false },
+    { id: 2, text: "Build a React app", completed: false },
+    { id: 3, text: "Deploy the React app", completed: false },
   ]);
 
   // Function to mark a todo as completed
